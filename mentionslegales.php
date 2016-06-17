1c82bf80625e23a88fd75a84f1ff460d2fbb3c16
edouard barbe <ebarbe@varibase.com>
2016-06-17 16:55:46 -0400

    Update des mentions légales

--- mentionslegales.php
+++ mentionslegales.php
@@ -38,16 +38,15 @@
         <h1 class="page-title">MENTIONS LEGALES</h1>
         
         <h3 class="subtitle">
-            Editeur des présents sites :
+            Editeur du présent site :
         </h3>
-        <p class="text">
-            www.jack-russell.net<br>www.jack-russell.fr<br>www.jack-russell.ca<br><br><br>(Ci-dessous appelés « les sites »)<br><br>Jack Russell., Société Anonyme au capital de 946 134 Euros, situé au 25, rue d’Hauteville – 75010 Paris, France, numéro SIREN 438 689 747<br>(Ci-dessous appelé « Jack-Russell »)<br>Pour toute suggestion, information, réaction concernant ce site, écrivez à  contact@jack-russell.net
+        <p class="text">www.jack-russell.fr<br><br><br>(Ci-dessous appelés « le site »)<br><br>Jack Russell., Société Anonyme au capital de 946 134 Euros, situé au 25, rue d’Hauteville – 75010 Paris, France, numéro SIREN 438 689 747<br>(Ci-dessous appelé « Jack-Russell »)<br>Pour toute suggestion, information, réaction concernant ce site, écrivez à  contact@jack-russell.fr
         </p><br><br>
         <h3 class="subtitle">
             Crédits :
         </h3>
         <p class="text">
-            Les  sites ont été réalisés par Jack-Russell<br>Direction de publication : Frédéric Goubet, Associé de Jack-Russell<br>Photos : Stocksy et Shutterstock
+            Le  sites a été réalisé par Jack-Russell<br>Direction de publication : Frédéric Goubet, Associé de Jack-Russell<br>Photos : Stocksy et Shutterstock
         </p><br><br>
             <h3 class="subtitle">
             Conception, Réalisation technique des sites : 
@@ -65,8 +64,8 @@
             Droits d'auteur :
         </h3>
         <p class="text">
-            Les sites sont protégés par le droit d'auteur. Les droits de propriété intellectuelle relatifs aux sites restent la propriété exclusive de Jack Russell. Les marques de l'éditeur des sites suivants<br>
-            www.jack-russell.net<br>www.jack-russell.fr<br>www.jack-russell.ca<br><br>
+            Le site est protégés par le droit d'auteur. Les droits de propriété intellectuelle relatifs aux sites restent la propriété exclusive de Jack Russell. Les marques de l'éditeur du site<br>
+           www.jack-russell.fr<br><br>
             ainsi que les logos figurant sur les sites sont des marques déposées. L'utilisation de ces sites ne confère aucun droit. En conséquence, le téléchargement de tout ou partie des textes, images, sons, photographies, données, marques et tout autre élément qui y sont contenus qu'à des fins strictement privées, est interdit.<br>
             Toute réutilisation, diffusion, commercialisation, reproduction et représentation, totale ou partielle, d'éléments de ces sites à d'autres fins qu'un usage privé, sans l'autorisation préalable expresse de Jack Russell, est interdite et constitutive du délit de contrefaçon de droit d'auteur et/ou d'atteinte aux droits voisins.
         </p><br><br>
@@ -74,46 +73,46 @@
             Accès aux sites :
         </h3>
         <p class="text">
-            L'utilisateur de ces sites reconnaît disposer de la compétence et des moyens nécessaires pour accéder et utiliser ces sites. Il est rappelé que le fait d'accéder ou de se maintenir frauduleusement dans un système informatique, d'entraver ou de fausser le fonctionnement d'un tel système, d'introduire ou de modifier frauduleusement des données dans un système informatique constituent des délits passibles de sanctions pénales.
+            L'utilisateur de ce site reconnaît disposer de la compétence et des moyens nécessaires pour accéder et utiliser ce site. Il est rappelé que le fait d'accéder ou de se maintenir frauduleusement dans un système informatique, d'entraver ou de fausser le fonctionnement d'un tel système, d'introduire ou de modifier frauduleusement des données dans un système informatique constituent des délits passibles de sanctions pénales.
         </p><br><br>
             <h3 class="subtitle">
-            Contenu des sites :
+            Contenu du site :
         </h3>
         <p class="text">
-            Jack Russell se réserve le droit de corriger, à tout moment et sans préavis, le contenu de ces sites. En outre, Jack Russell décline toute responsabilité en cas de retard, d'erreur ou d'omission quant au contenu des présentes pages de même qu'en cas d'interruption ou de non-disponibilité du service.
+            Jack Russell se réserve le droit de corriger, à tout moment et sans préavis, le contenu de ce site. En outre, Jack Russell décline toute responsabilité en cas de retard, d'erreur ou d'omission quant au contenu des présentes pages de même qu'en cas d'interruption ou de non-disponibilité du service.
         </p><br><br>
             <h3 class="subtitle">
             Liens hypertextes :
         </h3>
         <p class="text">
-            La création de liens hypertextes établis en direction d'autres sites à partir de Jack Russell ne sauraient, en aucun cas, engager la responsabilité de Jack Russell. <br>
-             www.jack-russell.net<br>www.jack-russell.fr<br>www.jack-russell.ca<br>
-            ne saurait, en aucun cas, engager la responsabilité de Jack Russell. Les liens hypertextes établis en direction d'autres sites à partir de Jack Russell  ne sauraient, en aucun cas, engager la responsabilité de Jack Russell.
+           Les liens hypertextes établis en direction d'autres sites à partir de <br>
+            www.jack-russell.fr<br>
+            ne saurait, en aucun cas, engager la responsabilité de Jack Russell.
         </p><br><br>
             <h3 class="subtitle">
             Cookies
         </h3>
         <p class="text">
-            Le Site implante deux types de cookie dans l’ordinateur de l’utilisateur.<br>
-            Cookie stockant des informations fournies par la personne concernée<br>
-        Ce cookie enregistre des informations relatives à la navigation de l’utilisateur sur le Site et stocke des informations que l’utilisateur a saisi durant sa visite (nom et adresse électronique). Ainsi, l’utilisateur n’aura pas besoin, lors de sa prochaine visite de les saisir à nouveau. Jack Russell pourra les consulter lors des prochaines visites de l’utilisateur. La durée de conservation de ces informations dans l’ordinateur de l’utilisateur est de deux semaines.<br>
-        L’utilisateur est informé qu’il peut s’opposer à l’enregistrement de « cookies » en configurant son navigateur de la manière suivante :<br>
-        - Pour Mozilla Firefox :<br>
-        Choisir le menu « outil » puis « Options »<br>
-        Cliquer sur l’icône « vie privée »<br>
-        Repérer le menu « cookie » et sélectionner les options qui conviennent<br>
-        - Pour Microsoft Internet Explorer :<br>
-        choisir le menu « Outils »puis « Options Internet »<br>
-        cliquer sur l’onglet « Confidentialité »<br>
-        sélectionner le niveau souhaité à l’aide du curseur.<br>
-        - Pour Opéra 6.0 et au-delà :<br>
-        choisir le menu « Fichier »> »Préférences »<br>
-        Vie Privée<br><br>
-            Cookie stockant des informations relatives à la navigation de l’utilisateur<br>
-En outre, ce Site utilise Google Analytics, un service d’analyse de site internet fourni par Google Inc. (ci-après dénommé « Google »). Google Analytics utilise des cookies pour aider Jack Russell à analyser l’utilisation du Site par ses utilisateurs. Les données générées par les cookies concernant l’utilisation du Site par l’utilisateur (y compris son adresse IP) seront transmises et stockées par Google sur des serveurs situés aux Etats-Unis. Google utilisera ces informations dans le but d’évaluer l’utilisation du Site par l’utilisateur, de compiler des rapports sur l’activité du Site à destination de Jack Russell et de fournir d’autres services relatifs à l’activité du Site et à l’utilisation d’Internet.<br>
-Google est susceptible de communiquer ces données à des tiers en cas d’obligation légale ou lorsque ces tiers traitent ces données pour le compte de Google, y compris notamment Jack Russell, l’éditeur de ce Site. Google ne recoupera pas l’adresse IP de l’utilisateur avec toute autre donnée détenue par Google.<br>
-En utilisant ce Site l’utilisateur consent expressément au traitement de ses données à caractère personnel par Google dans les conditions et pour les finalités décrites ci-dessus. La durée de conservation de ces informations dans l’ordinateur de l’utilisateur est de deux semaines.<br>
-Nous informons l’utilisateur qu’il peut désactiver l’utilisation de cookies en sélectionnant les paramètres appropriés de son navigateur. Cependant, une telle désactivation pourrait empêcher l’utilisation de certaines fonctionnalités de ce Site.<br>
+            Le site dépose deux types de cookie dans l’ordinateur de l’utilisateur.<br>
+           Cookie stockant des informations fournies par l’utilisateur :<br>
+       Ce cookie enregistre des informations relatives à la navigation de l’utilisateur sur le site et stocke les informations de contact que l’utilisateur a éventuellement saisi de son plein gré durant sa visite (nom, société, téléphone, email, sujet, message).<br>
+        <br><br>
+            Cookie stockant des informations relatives à la navigation de l’utilisateur :<br>
+Le site utilise Google Analytics, un service d’analyse de site internet fourni par Google Inc. (ci-après dénommé « Google »). Google Analytics utilise des cookies pour aider Jack Russell à analyser l’utilisation du site par ses utilisateurs. Les données générées par les cookies concernant l’utilisation du site par l’utilisateur (y compris son adresse IP) seront transmises et stockées par Google sur des serveurs situés aux Etats-Unis. Google utilisera ces informations dans le but d’évaluer l’utilisation du site par l’utilisateur, de compiler des rapports sur l’activité du site à destination de Jack Russell et de fournir d’autres services relatifs à l’activité du site et à l’utilisation d’Internet. Google est susceptible de communiquer ces données à des tiers en cas d’obligation légale ou lorsque ces tiers traitent ces données pour le compte de Google, y compris notamment Jack Russell, l’éditeur de ce site. Google ne recoupera pas l’adresse IP de l’utilisateur avec toute autre donnée détenue par Google. En utilisant ce site, l’utilisateur consent expressément au traitement de ses données à caractère personnel par Google dans les conditions et pour les finalités décrites ci-dessus. La durée de conservation de ces informations dans l’ordinateur de l’utilisateur est de deux semaines.<br>
+			<br>
+			Nous informons l’utilisateur qu’il peut désactiver l’utilisation des cookies en sélectionnant les paramètres appropriés de son navigateur de la manière suivante :<br>
+- Pour Mozilla Firefox :<br>
+Choisir le menu « outil » puis « Options »<br>
+Cliquer sur l’icône « vie privée »<br>
+Repérer le menu « cookie » et sélectionner les options qui conviennent<br>
+- Pour Microsoft Internet Explorer :<br>
+choisir le menu « Outils »puis « Options Internet »<br>
+cliquer sur l’onglet « Confidentialité »<br>
+sélectionner le niveau souhaité à l’aide du curseur.<br>
+- Pour Opéra 6.0 et au-delà :<br>
+choisir le menu « Fichier »> »Préférences »<br>
+Vie Privée<br>
+
 
         </p><br><br>
             
@@ -121,17 +120,17 @@ Nous informons l’utilisateur qu’il peut désactiver l’utilisation de cooki
             Protection des données personnelles : 
         </h3>
         <p class="text">
-           Jack Russell ne divulguera pas à des tiers les données personnelles vous concernant que vous pourriez lui communiquer par l’intermédiaire de ces sites. Elles ne seront utilisées que dans le but de vous répondre le plus efficacement possible. Les Sites ne sont pas destinés à recevoir de votre part des informations à caractère confidentiel. En conséquence et à l'exception des données personnelles visées ci-dessus, toute information, quelle qu'en soit la forme - document, donnée, graphique, question, suggestion, concept, remarque, ou autre -, que vous nous communiquerez sur les Sites ne sera en aucun cas tenue pour confidentielle. En conséquence, sa simple transmission par vos soins nous confère le droit de l'utiliser, de la reproduire, de la diffuser, de la modifier ou de la transmettre dans le but de traiter votre demande. Conformément à la loi française (article 27 de la Loi n° 78.17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés), vous disposez d'un droit d'opposition (art. 26) d'accès (art. 34 à 38), de rectification (art. 36) et de suppression des données personnelles qui vous concernent.
-           Vous pouvez exercer ce droit en vous adressant à contact@jack-russell.net
-           Vous pouvez également nous envoyer un courrier à l'adresse suivante :<br>
+           À l'exception des adresses IP, lesquelles sont abordées plus en détail dans la section Cookies ci-dessus, le site ne recueille pas automatiquement de données personnelles de l’utilisateur lors d’une visite sur le site. Les données personnelles ne sont recueillies, utilisées et divulguées que si elles sont communiquées de son plein gré par l’utilisateur. Cependant, Jack Russell ne divulguera pas à des tiers les données personnelles qui ont pu lui être communiquées par l’intermédiaire de ce site. Elles ne seront utilisées que dans le but de répondre le plus efficacement possible à l’utilisateur. Le site n’est pas destiné à recevoir de la part des utilisateurs des informations à caractère confidentiel. En conséquence et à l'exception des données personnelles visées ci-dessus, toute information, quelle qu'en soit la forme - document, donnée, graphique, question, suggestion, concept, remarque, ou autre -, qu’un utilisateur nous communiquera sur le site, ou via le site, ne sera en aucun cas tenue pour confidentielle; sa simple transmission par les soins de l’utilisateur nous confère le droit de l'utiliser, de la reproduire, de la diffuser, de la modifier ou de la transmettre dans le but de traiter la demande de l’utilisateur. Conformément à la loi française (article 27 de la Loi n° 78.17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés), vous disposez d'un droit d'opposition (art. 26) d'accès (art. 34 à 38), de rectification (art. 36) et de suppression des données personnelles qui vous concernent. Vous pouvez exercer ce droit en vous adressant à<br>
+			contact@jack-russell.fr<br>
+			Vous pouvez également nous envoyer un courrier à l'adresse suivante :<br>
            Jack Russell<br>25, Rue d'Hauteville<br>75010 Paris<br>France
         </p><br><br>
             <h3 class="subtitle">
             Lois applicables :  
         </h3>
         <p class="text">
-           Le contenu de ces sites est assujetti au droit applicable en France et tout utilisateur reconnaît la compétence des tribunaux de ce pays pour tout ce qui concerne le contenu et l'utilisation du site ou les recours en découlant<br><br>
-            Dernière mise à jour des conditions générales d'utilisation : 01/06/2016<br>
+           Le contenu de ce site est assujetti au droit applicable en France et tout utilisateur reconnaît la compétence des tribunaux de ce pays pour tout ce qui concerne le contenu et l'utilisation du site ou les recours en découlant<br><br>
+            Dernière mise à jour des conditions générales d'utilisation : 17/06/2016<br>
             &copy; Agence Jack Russell. Tous droits réservés.
             
         </p><br><br>
